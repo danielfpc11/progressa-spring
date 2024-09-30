d8513931eb26fbf12bacd798ed535286a989e265
Daniel Pulgarin <danielfpc11@gmail.com>
2024-09-30 14:43:26 +0200

    PROGRESSA-1 ExerciseDataPopulator and ExerciseModelPopulator created

---
+++ src/main/java/progressa/progressaspring/populators/exercise/ExerciseDataPopulator.java
@@ -0,0 +1,20 @@
+package progressa.progressaspring.populators.exercise;
+
+import org.springframework.util.Assert;
+import progressa.progressaspring.datas.ExerciseData;
+import progressa.progressaspring.populators.BasePopulator;
+
+/**
+ * @author danielfpc11@gmail.com
+ */
+public class ExerciseDataPopulator extends BasePopulator<ExerciseData, ExerciseData> {
+
+    @Override
+    public void populate(final ExerciseData exerciseDataSource, final ExerciseData exerciseDataTarget) {
+        Assert.notNull(exerciseDataSource, SOURCE_NOT_NULL_MESSAGE);
+        Assert.notNull(exerciseDataTarget, TARGET_NOT_NULL_MESSAGE);
+
+        exerciseDataTarget.setName(exerciseDataSource.getName());
+    }
+
+}
