e7039806f9c40b5d6aac1e5299f726a0af633784
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-01 10:41:32 +0200

    PROGRESSA-1 SetData created

---
+++ src/main/java/progressa/progressaspring/datas/SetData.java
@@ -0,0 +1,23 @@
+package progressa.progressaspring.datas;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author danielfpc11@gmail.com
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SetData {
+
+    private Long id;
+    private Integer number;
+    private Float weight;
+    private Integer repetitions;
+    private Integer rir;
+
+}
