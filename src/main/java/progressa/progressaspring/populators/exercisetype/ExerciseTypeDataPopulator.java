47b5c33f98dc2517855f5ea774a592670166da3d
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-01 12:36:46 +0200

    PROGRESSA-1 ExerciseDataPopulator and ExerciseModelPopulator renamed to ExerciseTypeDataPopulator and ExerciseTypeModelPopulator

---
+++ src/main/java/progressa/progressaspring/populators/exercisetype/ExerciseTypeDataPopulator.java
@@ -0,0 +1,20 @@
+package progressa.progressaspring.populators.exercisetype;
+
+import org.springframework.util.Assert;
+import progressa.progressaspring.datas.ExerciseTypeData;
+import progressa.progressaspring.populators.BasePopulator;
+
+/**
+ * @author danielfpc11@gmail.com
+ */
+public class ExerciseTypeDataPopulator extends BasePopulator<ExerciseTypeData, ExerciseTypeData> {
+
+    @Override
+    public void populate(final ExerciseTypeData exerciseTypeDataSource, final ExerciseTypeData exerciseTypeDataTarget) {
+        Assert.notNull(exerciseTypeDataSource, SOURCE_NOT_NULL_MESSAGE);
+        Assert.notNull(exerciseTypeDataTarget, TARGET_NOT_NULL_MESSAGE);
+
+        exerciseTypeDataTarget.setName(exerciseTypeDataSource.getName());
+    }
+
+}
