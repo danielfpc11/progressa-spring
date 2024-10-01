199443fd72642964d280516565bfea861d53254a
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-01 10:54:07 +0200

    PROGRESSA-1 Added some assertions in DefaultSetServiceImpl

--- src/main/java/progressa/progressaspring/services/impl/DefaultSetServiceImpl.java
+++ src/main/java/progressa/progressaspring/services/impl/DefaultSetServiceImpl.java
@@ -1,7 +1,9 @@
 package progressa.progressaspring.services.impl;
 
 import jakarta.annotation.Resource;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import progressa.progressaspring.models.SetModel;
 import progressa.progressaspring.repositories.SetRepository;
 import progressa.progressaspring.services.SetService;
@@ -14,6 +16,10 @@ import java.util.Optional;
  */
 @Service
 public class DefaultSetServiceImpl implements SetService {
+
+    private static final String ID_NOT_NULL_MESSAGE = "Id must not be null.";
+    private static final String ID_POSITIVE_MESSAGE = "Id must be greater than zero.";
+    private static final String SET_NOT_NULL_MESSAGE = "Set must not be null.";
 
     @Resource
     private SetRepository setRepository;
@@ -30,12 +36,19 @@ public class DefaultSetServiceImpl implements SetService {
 
     @Override
     public void deleteById(final Long id) throws IllegalArgumentException {
+        assertIdNotNullAndPositive(id);
         setRepository.deleteById(id);
     }
 
     @Override
     public SetModel save(final SetModel setModel) throws IllegalArgumentException {
+        Assert.notNull(setModel, SET_NOT_NULL_MESSAGE);
         return setRepository.save(setModel);
+    }
+
+    private void assertIdNotNullAndPositive(final Long id) throws IllegalArgumentException {
+        Assert.notNull(id, ID_NOT_NULL_MESSAGE);
+        Assert.isTrue(id > NumberUtils.LONG_ZERO, ID_POSITIVE_MESSAGE);
     }
 
 }
