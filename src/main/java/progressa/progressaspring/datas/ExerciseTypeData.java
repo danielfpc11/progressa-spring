7a6b163b338f15b99d8ebaa2a3e4dc6f392bbac9
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-01 12:31:06 +0200

    PROGRESSA-1 ExerciseData renamed to ExerciseTypeData

--- src/main/java/progressa/progressaspring/datas/ExerciseTypeData.java
+++ src/main/java/progressa/progressaspring/datas/ExerciseTypeData.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ExerciseData {
+public class ExerciseTypeData {
 
     private Long id;
     private String name;
