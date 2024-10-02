cc28f9471d9c855276fe121491201fb89edfdc05
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-02 12:19:33 +0200

    PROGRESSA-1 ExerciseTypeConverter and ExerciseTypeReverseConverter updated to add relationship between Exercise and ExerciseType

--- src/main/java/progressa/progressaspring/converters/exercisetype/ExerciseTypeConverter.java
+++ src/main/java/progressa/progressaspring/converters/exercisetype/ExerciseTypeConverter.java
@@ -1,14 +1,20 @@
 package progressa.progressaspring.converters.exercisetype;
 
+import lombok.AllArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
+import progressa.progressaspring.datas.ExerciseData;
 import progressa.progressaspring.datas.ExerciseTypeData;
+import progressa.progressaspring.models.ExerciseModel;
 import progressa.progressaspring.models.ExerciseTypeModel;
 import progressa.progressaspring.utils.AssertUtils;
 
 /**
  * @author danielfpc11@gmail.com
  */
+@AllArgsConstructor
 public class ExerciseTypeConverter implements Converter<ExerciseTypeModel, ExerciseTypeData> {
+
+    private final Converter<ExerciseModel, ExerciseData> exerciseConverter;
 
     @Override
     public ExerciseTypeData convert(final ExerciseTypeModel exerciseTypeModel) {
@@ -16,6 +22,10 @@ public class ExerciseTypeConverter implements Converter<ExerciseTypeModel, Exerc
         return ExerciseTypeData.builder()
                                .id(exerciseTypeModel.getId())
                                .name(exerciseTypeModel.getName())
+                               .exerciseDatas(exerciseTypeModel.getExerciseModels()
+                                                               .stream()
+                                                               .map(exerciseConverter::convert)
+                                                               .toList())
                                .build();
     }
 
--- src/main/java/progressa/progressaspring/converters/exercisetype/ExerciseTypeReverseConverter.java
+++ src/main/java/progressa/progressaspring/converters/exercisetype/ExerciseTypeReverseConverter.java
@@ -1,14 +1,20 @@
 package progressa.progressaspring.converters.exercisetype;
 
+import lombok.AllArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
+import progressa.progressaspring.datas.ExerciseData;
 import progressa.progressaspring.datas.ExerciseTypeData;
+import progressa.progressaspring.models.ExerciseModel;
 import progressa.progressaspring.models.ExerciseTypeModel;
 import progressa.progressaspring.utils.AssertUtils;
 
 /**
  * @author danielfpc11@gmail.com
  */
+@AllArgsConstructor
 public class ExerciseTypeReverseConverter implements Converter<ExerciseTypeData, ExerciseTypeModel> {
+
+    private final Converter<ExerciseData, ExerciseModel> exerciseReverseConverter;
 
     @Override
     public ExerciseTypeModel convert(final ExerciseTypeData exerciseTypeData) {
@@ -16,6 +22,10 @@ public class ExerciseTypeReverseConverter implements Converter<ExerciseTypeData,
         return ExerciseTypeModel.builder()
                                 .id(exerciseTypeData.getId())
                                 .name(exerciseTypeData.getName())
+                                .exerciseModels(exerciseTypeData.getExerciseDatas()
+                                                                .stream()
+                                                                .map(exerciseReverseConverter::convert)
+                                                                .toList())
                                 .build();
     }
 
