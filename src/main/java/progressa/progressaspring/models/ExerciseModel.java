60c242d6cb374dc2219b3c16968dc9daf9bd630f
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-02 11:26:47 +0200

    PROGRESSA-1 Relationship between ExerciseModel and WorkoutModel created

--- src/main/java/progressa/progressaspring/models/ExerciseModel.java
+++ src/main/java/progressa/progressaspring/models/ExerciseModel.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,5 +26,9 @@ public class ExerciseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_id")
+    private WorkoutModel workoutModel;
 
 }
--- src/main/java/progressa/progressaspring/models/WorkoutModel.java
+++ src/main/java/progressa/progressaspring/models/WorkoutModel.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author danielfpc11@gmail.com
@@ -29,5 +31,8 @@ public class WorkoutModel {
 
     @Column(name = "date", nullable = false)
     private Date date;
+
+    @OneToMany(mappedBy = "workoutModel")
+    private List<ExerciseModel> exerciseModels;
 
 }
