dedd9f7ea70c5ec171ea107f2a920dde3a2a1aeb
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-02 11:07:56 +0200

    PROGRESSA-1 ExerciseData created

---
+++ src/main/java/progressa/progressaspring/datas/ExerciseData.java
@@ -0,0 +1,19 @@
+package progressa.progressaspring.datas;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author danielfpc11@gmail.com
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExerciseData {
+
+    private Long id;
+
+}
