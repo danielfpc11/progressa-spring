8675591037e84bc27db51bec6eb5a7ed881f8088
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-03 14:00:14 +0200

    PROGRESSA-1 ExerciseTypeFacade javadoc added

--- src/main/java/progressa/progressaspring/facades/ExerciseTypeFacade.java
+++ src/main/java/progressa/progressaspring/facades/ExerciseTypeFacade.java
@@ -7,16 +7,41 @@ import java.util.Optional;
 
 /**
  * @author danielfpc11@gmail.com
- * TODO: javadoc
  */
 public interface ExerciseTypeFacade {
 
+    /**
+     * Retrieves all exercise type data entries.
+     *
+     * @return a list of all exercise type data entries.
+     */
     List<ExerciseTypeData> findAll();
 
+    /**
+     * Finds an exercise type data entry by its id.
+     *
+     * @param id the id of the exercise type data to retrieve.
+     * @return an optional containing the found exercise type data entry,
+     *         or an empty optional if no entry is found.
+     * @throws IllegalArgumentException if the provided id is null or invalid.
+     */
     Optional<ExerciseTypeData> findById(final Long id) throws IllegalArgumentException;
 
+    /**
+     * Deletes an exercise type data entry by its id.
+     *
+     * @param id the id of the exercise type data to delete.
+     * @throws IllegalArgumentException if the provided id is null or invalid.
+     */
     void deleteById(final Long id) throws IllegalArgumentException;
 
+    /**
+     * Saves an exercise type data entry.
+     *
+     * @param exerciseTypeData the exercise type data entry to save.
+     * @return the saved exercise type data entry.
+     * @throws IllegalArgumentException if the provided data is null or invalid.
+     */
     ExerciseTypeData save(final ExerciseTypeData exerciseTypeData) throws IllegalArgumentException;
 
 }
