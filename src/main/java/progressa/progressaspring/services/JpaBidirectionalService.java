383cfc174ca631060e205b47252ef8ed7df484a6
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-03 14:08:10 +0200

    PROGRESSA-1 JpaBidirectionalService added javadoc

--- src/main/java/progressa/progressaspring/services/JpaBidirectionalService.java
+++ src/main/java/progressa/progressaspring/services/JpaBidirectionalService.java
@@ -6,12 +6,24 @@ import progressa.progressaspring.models.WorkoutModel;
 
 /**
  * @author danielfpc11@gmail.com
- * TODO: javadoc
  */
 public interface JpaBidirectionalService {
 
+
+    /**
+     * Sets the relationships for the given workout model, ensuring all
+     * associated entities are correctly linked.
+     *
+     * @param workoutModel the workout model for which relationships are to be set, must not be null.
+     */
     void setWorkoutRelationships(@NonNull final WorkoutModel workoutModel);
 
+    /**
+     * Sets the relationships for the given exercise model, ensuring all
+     * associated entities are correctly linked.
+     *
+     * @param exerciseModel the exercise model for which relationships are to be set, must not be null.
+     */
     void setExerciseRelationships(@NonNull final ExerciseModel exerciseModel);
 
 }
