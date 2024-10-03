7ee467924440756697820f7632c300b5aa48d4be
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-03 15:35:28 +0200

    PROGRESSA-1 Removed unnecessary import

--- src/test/java/progressa/progressaspring/converters/exercisetype/ExerciseTypeConverterTest.java
+++ src/test/java/progressa/progressaspring/converters/exercisetype/ExerciseTypeConverterTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
