d6a39ecea52de30652bc5ce91291ac1e76524c7b
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-03 19:19:04 +0200

    PROGRESSA-1 Removed spring-config.xml

--- src/main/java/progressa/progressaspring/ProgressaSpringApplication.java
+++ src/main/java/progressa/progressaspring/ProgressaSpringApplication.java
@@ -2,10 +2,8 @@ package progressa.progressaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:spring/spring-config.xml")
 public class ProgressaSpringApplication {
 
 	public static void main(final String[] args) {
