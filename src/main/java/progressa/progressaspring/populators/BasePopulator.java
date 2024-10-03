93b8cebd91357b603b610df9f6a0b342d1e4988c
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-03 14:04:49 +0200

    PROGRESSA-1 BasePopulator added javadoc

--- src/main/java/progressa/progressaspring/populators/BasePopulator.java
+++ src/main/java/progressa/progressaspring/populators/BasePopulator.java
@@ -2,13 +2,18 @@ package progressa.progressaspring.populators;
 
 /**
  * @author danielfpc11@gmail.com
- * TODO: javadoc
  */
 public abstract class BasePopulator<S, T> {
 
     protected static final String SOURCE_NOT_NULL_MESSAGE = "Source must not be null.";
     protected static final String TARGET_NOT_NULL_MESSAGE = "Target must not be null.";
 
+    /**
+     * Populates the target object with data from the source object.
+     *
+     * @param source the source object containing the data.
+     * @param target the target object to populate with data.
+     */
     public abstract void populate(S source, T target);
 
 }
