35c5ee0b8e8ff9cf4d6da05e72cece53bf2b1fc7
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-03 14:03:50 +0200

    PROGRESSA-1 WorkoutFacade javadoc added

--- src/main/java/progressa/progressaspring/facades/WorkoutFacade.java
+++ src/main/java/progressa/progressaspring/facades/WorkoutFacade.java
@@ -6,16 +6,41 @@ import java.util.Optional;
 
 /**
  * @author danielfpc11@gmail.com
- * TODO: javadoc
  */
 public interface WorkoutFacade {
 
+    /**
+     * Retrieves all workout data entries.
+     *
+     * @return a list of all workout data entries.
+     */
     List<WorkoutData> findAll();
 
+    /**
+     * Finds a workout data entry by its id.
+     *
+     * @param id the id of the workout data to retrieve.
+     * @return an optional containing the found workout data entry,
+     *         or an empty optional if no entry is found.
+     * @throws IllegalArgumentException if the provided id is null or invalid.
+     */
     Optional<WorkoutData> findById(final Long id) throws IllegalArgumentException;
 
+    /**
+     * Deletes a workout data entry by its id.
+     *
+     * @param id the id of the workout data to delete.
+     * @throws IllegalArgumentException if the provided id is null or invalid.
+     */
     void deleteById(final Long id) throws IllegalArgumentException;
 
+    /**
+     * Saves a workout data entry.
+     *
+     * @param workoutData the workout data entry to save.
+     * @return the saved workout data entry.
+     * @throws IllegalArgumentException if the provided data is null or invalid.
+     */
     WorkoutData save(final WorkoutData workoutData) throws IllegalArgumentException;
 
 }
