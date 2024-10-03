d286299accb0ca97e937ddac340d1ec70d67464d
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-03 14:06:04 +0200

    PROGRESSA-1 ExerciseService added javadoc

--- src/main/java/progressa/progressaspring/services/ExerciseService.java
+++ src/main/java/progressa/progressaspring/services/ExerciseService.java
@@ -6,16 +6,41 @@ import java.util.Optional;
 
 /**
  * @author danielfpc11@gmail.com
- * TODO: javadoc
  */
 public interface ExerciseService {
 
+    /**
+     * Retrieves all exercise model entries.
+     *
+     * @return a list of all exercise model entries.
+     */
     List<ExerciseModel> findAll();
 
+    /**
+     * Finds an exercise model entry by its id.
+     *
+     * @param id the id of the exercise model to retrieve.
+     * @return an optional containing the found exercise model entry,
+     *         or an empty optional if no entry is found.
+     * @throws IllegalArgumentException if the provided id is null or invalid.
+     */
     Optional<ExerciseModel> findById(final Long id) throws IllegalArgumentException;
 
+    /**
+     * Deletes an exercise model entry by its id.
+     *
+     * @param id the id of the exercise model to delete.
+     * @throws IllegalArgumentException if the provided id is null or invalid.
+     */
     void deleteById(final Long id) throws IllegalArgumentException;
 
+    /**
+     * Saves an exercise model entry.
+     *
+     * @param exerciseModel the exercise model entry to save.
+     * @return the saved exercise model entry.
+     * @throws IllegalArgumentException if the provided data is null or invalid.
+     */
     ExerciseModel save(final ExerciseModel exerciseModel) throws IllegalArgumentException;
 
 }
