464c30139c25a2a96ab3c63941ca580ce745049f
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-03 21:23:44 +0200

    PROGRESSA-1 BaseTest formatted

--- src/test/java/progressa/progressaspring/BaseTest.java
+++ src/test/java/progressa/progressaspring/BaseTest.java
@@ -13,8 +13,8 @@ public abstract class BaseTest {
      * and that the exception message matches the expected message.
      *
      * @param exceptionClass The class of the expected exception to be thrown.
-     * @param message       The expected message of the exception that should be thrown.
-     * @param executable    The executable code block expected to throw the exception.
+     * @param message        The expected message of the exception that should be thrown.
+     * @param executable     The executable code block expected to throw the exception.
      */
     protected void assertException(final Class<? extends Throwable> exceptionClass,
                                    final String message,
