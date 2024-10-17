ae9e9b5a56c9505e54fa4b1b1fa88b62c820857c
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-17 11:56:56 +0200

    PROGRESSA-3 Created SetDataRelationshipsPopulator

--- src/main/java/progressa/progressaspring/populators/set/SetDataRelationshipsPopulator.java
+++ src/main/java/progressa/progressaspring/populators/set/SetDataRelationshipsPopulator.java
@@ -9,17 +9,14 @@ import progressa.progressaspring.populators.BasePopulator;
  * @author danielfpc11@gmail.com
  */
 @Component
-public class SetDataPopulator extends BasePopulator<SetData, SetData> {
+public class SetDataRelationshipsPopulator extends BasePopulator<SetData, SetData> {
 
     @Override
     public void populate(final SetData setDataSource, final SetData setDataTarget) {
         Assert.notNull(setDataSource, SOURCE_NOT_NULL_MESSAGE);
         Assert.notNull(setDataTarget, TARGET_NOT_NULL_MESSAGE);
 
-        setDataTarget.setNumber(setDataSource.getNumber());
-        setDataTarget.setWeight(setDataSource.getWeight());
-        setDataTarget.setRepetitions(setDataSource.getRepetitions());
-        setDataTarget.setRir(setDataSource.getRir());
+        setDataTarget.setExerciseId(setDataSource.getExerciseId());
     }
 
 }
