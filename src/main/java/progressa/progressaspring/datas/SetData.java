4ceedbef3052f56e6bfecb769cdec58763516168
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-17 12:40:32 +0200

    PROGRESSA-3 Added validation for SetData

--- src/main/java/progressa/progressaspring/datas/SetData.java
+++ src/main/java/progressa/progressaspring/datas/SetData.java
@@ -1,9 +1,15 @@
 package progressa.progressaspring.datas;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import progressa.progressaspring.groups.set.CreateSetGroup;
+import progressa.progressaspring.groups.set.UpdateSetGroup;
 
 /**
  * @author danielfpc11@gmail.com
@@ -14,11 +20,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SetData {
 
+    @Null(groups = CreateSetGroup.class)
+    @Positive
     private Long id;
+
+    @NotNull
+    @Positive
     private Integer number;
+
+    @NotNull
+    @Positive
     private Float weight;
+
+    @NotNull
+    @Positive
     private Integer repetitions;
+
+    @NotNull
+    @PositiveOrZero
     private Integer rir;
+
+    @NotNull(groups = UpdateSetGroup.class)
+    @Positive
     private Long exerciseId;
 
 }
