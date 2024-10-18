26313a7cce9510673b35c2e86ae8d2feef31dfaa
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-18 20:47:00 +0200

    PROGRESSA-3 Removed unnecessary import

--- src/main/java/progressa/progressaspring/datas/WorkoutData.java
+++ src/main/java/progressa/progressaspring/datas/WorkoutData.java
@@ -1,6 +1,5 @@
 package progressa.progressaspring.datas;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Positive;
