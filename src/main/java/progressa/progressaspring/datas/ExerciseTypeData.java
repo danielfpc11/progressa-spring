f9920ea4ef5a1f62cae43eed19c53efa332e2db5
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-18 20:14:45 +0200

    PROGRESSA-3 Validation added to ExerciseTypeData

--- src/main/java/progressa/progressaspring/datas/ExerciseTypeData.java
+++ src/main/java/progressa/progressaspring/datas/ExerciseTypeData.java
@@ -1,9 +1,15 @@
 package progressa.progressaspring.datas;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import progressa.progressaspring.groups.exercisetype.CreateExerciseTypeGroup;
+import progressa.progressaspring.groups.exercisetype.UpdateExerciseTypeGroup;
 
 import java.util.List;
 
@@ -16,8 +22,14 @@ import java.util.List;
 @NoArgsConstructor
 public class ExerciseTypeData {
 
+    @Null(groups = CreateExerciseTypeGroup.class)
+    @Positive
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @NotNull(groups = UpdateExerciseTypeGroup.class)
     private List<ExerciseData> exerciseDatas;
 
 }
