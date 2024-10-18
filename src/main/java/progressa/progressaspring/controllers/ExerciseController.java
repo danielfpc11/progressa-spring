ddf877fbff1d3e0d2d855f49540c57fc733657b4
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-18 20:45:42 +0200

    PROGRESSA-3 Validation added to ExerciseController

--- src/main/java/progressa/progressaspring/controllers/ExerciseController.java
+++ src/main/java/progressa/progressaspring/controllers/ExerciseController.java
@@ -1,11 +1,14 @@
 package progressa.progressaspring.controllers;
 
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,7 +16,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import progressa.progressaspring.datas.ExerciseData;
 import progressa.progressaspring.facades.ExerciseFacade;
+import progressa.progressaspring.groups.exercise.CreateExerciseGroup;
+import progressa.progressaspring.groups.exercise.UpdateExerciseGroup;
 import progressa.progressaspring.populators.BasePopulator;
+import progressa.progressaspring.utils.PopulatorUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +35,8 @@ public class ExerciseController {
     private ExerciseFacade exerciseFacade;
     @Resource
     private BasePopulator<ExerciseData, ExerciseData> exerciseDataPopulator;
+    @Resource
+    private BasePopulator<ExerciseData, ExerciseData> exerciseDataRelationshipsPopulator;
 
     @GetMapping("/all")
     public ResponseEntity<List<ExerciseData>> findAll() {
@@ -43,7 +51,7 @@ public class ExerciseController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<Object> saveNew(@RequestBody final ExerciseData exerciseData) {
+    public ResponseEntity<Object> saveNew(@RequestBody @Validated({CreateExerciseGroup.class}) final ExerciseData exerciseData) {
         return Optional.ofNullable(exerciseData)
                        .map(exercise -> exerciseFacade.save(exercise))
                        .map(exercise -> ResponseEntity.status(HttpStatus.CREATED).build())
@@ -51,20 +59,33 @@ public class ExerciseController {
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Object> saveUpdate(@PathVariable final Long id, @RequestBody final ExerciseData exerciseData) {
-        return exerciseFacade.findById(id)
-                             .map(exerciseDataFound -> {
-                                 exerciseDataPopulator.populate(exerciseData, exerciseDataFound);
-                                 return exerciseFacade.save(exerciseDataFound);
-                             })
-                             .map(exerciseDataFound -> ResponseEntity.ok().build())
-                             .orElseThrow();
+    public ResponseEntity<Object> saveUpdate(@PathVariable final Long id,
+                                             @RequestBody @Validated({UpdateExerciseGroup.class}) final ExerciseData exerciseData) {
+       return saveUpdate(id, exerciseData, List.of(exerciseDataPopulator, exerciseDataRelationshipsPopulator));
+    }
+
+    @PatchMapping("/patch/{id}")
+    public ResponseEntity<Object> savePatch(@PathVariable final Long id,
+                                            @RequestBody @Valid final ExerciseData exerciseData) {
+        return saveUpdate(id, exerciseData, List.of(exerciseDataPopulator));
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Object> deleteById(@PathVariable final Long id) {
         exerciseFacade.deleteById(id);
         return ResponseEntity.ok().build();
+    }
+
+    private ResponseEntity<Object> saveUpdate(final Long id,
+                                              final ExerciseData exerciseData,
+                                              final List<BasePopulator<ExerciseData, ExerciseData>> exerciseDataPopulators) {
+        return exerciseFacade.findById(id)
+                             .map(exerciseDataFound -> {
+                                 PopulatorUtils.populateExerciseDatas(exerciseData, exerciseDataFound, exerciseDataPopulators);
+                                 return exerciseFacade.save(exerciseDataFound);
+                             })
+                             .map(exerciseDataFound -> ResponseEntity.ok().build())
+                             .orElseThrow();
     }
 
 }
