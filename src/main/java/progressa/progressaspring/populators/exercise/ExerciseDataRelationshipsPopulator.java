2c750ed90ef1414aeea163c76d98d551383ebd20
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-18 20:39:35 +0200

    PROGRESSA-3 ExerciseDataRelationshipsPopulator created

--- src/main/java/progressa/progressaspring/populators/exercise/ExerciseDataRelationshipsPopulator.java
+++ src/main/java/progressa/progressaspring/populators/exercise/ExerciseDataRelationshipsPopulator.java
@@ -9,16 +9,14 @@ import progressa.progressaspring.populators.BasePopulator;
  * @author danielfpc11@gmail.com
  */
 @Component
-public class ExerciseDataPopulator extends BasePopulator<ExerciseData, ExerciseData> {
+public class ExerciseDataRelationshipsPopulator extends BasePopulator<ExerciseData, ExerciseData> {
 
     @Override
     public void populate(final ExerciseData exerciseDataSource, final ExerciseData exerciseDataTarget) {
         Assert.notNull(exerciseDataSource, SOURCE_NOT_NULL_MESSAGE);
         Assert.notNull(exerciseDataTarget, TARGET_NOT_NULL_MESSAGE);
 
-        exerciseDataTarget.setWorkoutId(exerciseDataSource.getWorkoutId());
-        exerciseDataTarget.setExerciseTypeId(exerciseDataSource.getExerciseTypeId());
-        exerciseDataTarget.setExerciseTypeName(exerciseDataSource.getExerciseTypeName());
+        exerciseDataTarget.setSetDatas(exerciseDataSource.getSetDatas());
     }
 
 }
