360a6d28208f6906b26fff12950e0cfb3bfd000b
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-18 19:58:06 +0200

    PROGRESSA-3 Validation added to WorkoutData

--- src/main/java/progressa/progressaspring/datas/WorkoutData.java
+++ src/main/java/progressa/progressaspring/datas/WorkoutData.java
@@ -1,9 +1,15 @@
 package progressa.progressaspring.datas;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import progressa.progressaspring.groups.workout.CreateWorkoutGroup;
+import progressa.progressaspring.groups.workout.UpdateWorkoutGroup;
 
 import java.util.Date;
 import java.util.List;
@@ -17,8 +23,14 @@ import java.util.List;
 @NoArgsConstructor
 public class WorkoutData {
 
+    @Null(groups = CreateWorkoutGroup.class)
+    @Positive
     private Long id;
+
+    @NotNull
     private Date date;
+
+    @NotNull(groups = UpdateWorkoutGroup.class)
     private List<ExerciseData> exerciseDatas;
 
 }
